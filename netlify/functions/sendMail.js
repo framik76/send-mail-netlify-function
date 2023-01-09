a6c3028a29d891a4f7f9bf7ed4bd1f6d7e29a498
francesco manenti <francesco@manenti.net>
2023-01-09 13:22:00 +0100

    change function to manage cors

--- netlify/functions/sendMail.js
+++ netlify/functions/sendMail.js
@@ -13,28 +13,42 @@ const transporter = nodemailer.createTransport(
 exports.handler = async function (event, context) {
     try {
         const { destination, sender, subject, text } =  JSON.parse(event.body);
-        console.log(`Sending mail to ${destination}`);
-    
-        const info = await transporter.sendMail({
-            from: sender,
-            to: destination,
-            subject: subject,
-            text: text,
-            /*attachments: [+ err.message 
-            {
-                filename: `report-${new Date().toDateString()}.pdf`,
-                content: report,
-                contentType: "application/pdf",
-            },
-            ],*/
-        });
+        
+        const headers = {
+            'Access-Control-Allow-Origin': 'https://casavacanzelavecchiafonte.it/',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+        };
 
-        if (info && info.messageId) {
-            console.log(`mail sent: ${info.messageId}`);
+        if (event.httpMethod === 'OPTIONS') {
+            // To enable CORS
             return {
-                statusCode: 200,
-                body: JSON.stringify({ message: info.messageId }),
+              statusCode: 200, // <-- Important!
+              headers,
+              body: 'This was a preflight call!'
             };
+        } else {
+            console.log(`Sending mail to ${destination}`);
+            const info = await transporter.sendMail({
+                from: sender,
+                to: destination,
+                subject: subject,
+                text: text,
+                /*attachments: [+ err.message 
+                {
+                    filename: `report-${new Date().toDateString()}.pdf`,
+                    content: report,
+                    contentType: "application/pdf",
+                },
+                ],*/
+            });
+            if (info && info.messageId) {
+                console.log(`mail sent: ${info.messageId}`);
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ message: info.messageId }),
+                };
+            }
         }
     } catch (err) {
         if (err.message === 'Bad Request') {
