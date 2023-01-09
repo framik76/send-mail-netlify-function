1d4efcfc881be1b6e16d8ab1e22105ad2c7a78a9
francesco manenti <francesco@manenti.net>
2023-01-09 11:00:50 +0100

    initial commit

---
+++ netlify/functions/sendMail.js
@@ -0,0 +1,52 @@
+const nodemailer = require("nodemailer");
+const mg = require("nodemailer-mailgun-transport");
+
+const transporter = nodemailer.createTransport(
+  mg({
+    auth: {
+      api_key: process.env.MAILGUN_API_KEY,
+      domain: process.env.MAILGUN_DOMAIN,
+    },
+  })
+);
+
+exports.handler = async function (event, context) {
+    try {
+        const { destination, sender, subject, text } =  JSON.parse(event.body);
+        console.log(`Sending mail to ${destination}`);
+    
+        const info = await transporter.sendMail({
+            from: sender,
+            to: destination,
+            subject: subject,
+            text: text,
+            /*attachments: [+ err.message 
+            {
+                filename: `report-${new Date().toDateString()}.pdf`,
+                content: report,
+                contentType: "application/pdf",
+            },
+            ],*/
+        });
+
+        if (info && info.messageId) {
+            console.log(`mail sent: ${info.messageId}`);
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ message: info.messageId }),
+            };
+        }
+    } catch (err) {
+        if (err.message === 'Bad Request') {
+            return {
+                statusCode: 409,
+                body: JSON.stringify({ message: 'error sending mail: check the payload of the request' }),
+            };    
+        } else {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: 'error sending mail: ' + err.message }),
+            };
+        }
+    }
+  };
